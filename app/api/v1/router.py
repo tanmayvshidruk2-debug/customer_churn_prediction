8d3cd4074424f6f9f831c0812168009db06baf7c
feat2123 <tanmayvshidruk2@gmail.com>
2026-02-28 07:33:36 -0800

    updated portion

--- app/api/v1/router.py
+++
@@ -1,6 +0,0 @@
-from fastapi import APIRouter
-from app.api.v1.endpoints import predict, health
-
-router = APIRouter()
-router.include_router(predict.router, tags=["Prediction"])
-router.include_router(health.router, tags=["Health"])
